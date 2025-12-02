90de2bc445591f5531587d7ab030b56aa9556b84
White Owl <161154637+TheTrueWhiteOwl@users.noreply.github.com>
2025-12-02 12:48:32 +0000

    feat(day-1): complete part 1

--- day-1/src/main.rs
+++ day-1/src/main.rs
@@ -1,3 +1,38 @@
+enum Direction {
+    North,
+    East,
+    South,
+    West,
+}
+
+const STARTING_DIRECTION: Direction = Direction::North;
+
+impl Direction {
+    const fn rotate_clockwise(&self) -> Self {
+        match self {
+            Direction::North => Direction::East,
+            Direction::East => Direction::South,
+            Direction::South => Direction::West,
+            Direction::West => Direction::North,
+        }
+    }
+
+    const fn rotate_anticlockwise(&self) -> Self {
+        match self {
+            Direction::North => Direction::West,
+            Direction::East => Direction::North,
+            Direction::South => Direction::East,
+            Direction::West => Direction::South,
+        }
+    }
+}
+
+struct Position {
+    x: isize,
+    y: isize,
+}
+
+
 fn main() {
     let args: Vec<String> = std::env::args().collect();
     let input_filepath = args.get(1).expect("Must pass at least one parameter for the filepath");
@@ -14,7 +49,25 @@ fn main() {
 }
 
 fn part1(input: String) {
-    todo!()
+    let input = input.trim();
+    let mut direction = STARTING_DIRECTION;
+    let mut position = Position { x: 0, y: 0 };
+    for instruction in input.split(", ") {
+        match &instruction[..1] {
+            "L" => direction = direction.rotate_anticlockwise(),
+            "R" => direction = direction.rotate_clockwise(),
+            _ => panic!("This should never be the case"),
+        }
+        let steps: isize = instruction[1..].parse().expect("This will always be a valid number");
+        match direction {
+            Direction::North => position.y += steps,
+            Direction::East => position.x += steps,
+            Direction::South => position.y -= steps,
+            Direction::West => position.x -= steps,
+        }
+    }
+
+    println!("{}", position.x.abs() + position.y.abs());
 }
 
 fn part2(input: String) {
